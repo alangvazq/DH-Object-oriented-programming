9f5ad534132bc7fbbe71b65f331cbb77d799117a
alangvazq <alan.g.vazq@gmail.com>
2024-02-08 18:54:17 -0600

    Clase 02 ejercicios

---
+++ Clase02-Introduccion-Java/src/presencial/Main.java
@@ -0,0 +1,34 @@
+package presencial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inscripciones a exámenes CTD");
+        System.out.println("Ingrese nombre: ");
+        System.out.println();
+        String nombre = scanner.nextLine();
+        char letraInicial = primeraLetra(nombre);
+        char letraFinal = ultimaLetra(nombre);
+        System.out.println("Ingrese matrícula númerica: ");
+        Integer matricula = scanner.nextInt();
+        System.out.println("Ingrese edad: ");
+        int edad = scanner.nextInt();
+        System.out.println("Ingrese nota anterior: ");
+        double nota = scanner.nextDouble();
+
+        System.out.println("Verifique datos ingresados: ");
+        System.out.println(nombre+" ,"+matricula+" ,"+edad+" ,"+nota);
+        System.out.println("Letra inicial: "+letraInicial+" Letra final: "+letraFinal);
+    }
+
+    public static char primeraLetra(String nombre){
+        return nombre.charAt(0);
+    }
+
+    public static char ultimaLetra(String nombre){
+        return nombre.charAt(nombre.length()-1);
+    }
+
+}
